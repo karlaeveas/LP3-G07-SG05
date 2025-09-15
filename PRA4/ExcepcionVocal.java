24a49d8d999b644c143072c88baf0f2ff0cae2db
Alexanderandres23 <rt.andres23@gmail.com>
2025-09-14 22:08:05 -0500

    Delete PRA4/ExcepcionVocal.java

--- PRA4/ExcepcionVocal.java
+++
@@ -1,9 +0,0 @@
-package ejerciciouno;
-
-public class ExcepcionVocal extends Exception {
-	private static final long serialVersionUID = 1L;
-
-    public ExcepcionVocal(char c) {
-        super(" Se ingresó una vocal: " + c);
-    }
-}
