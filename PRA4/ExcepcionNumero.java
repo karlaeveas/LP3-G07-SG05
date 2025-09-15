433950b817bb9c8010635a855122bf180092d587
Alexanderandres23 <rt.andres23@gmail.com>
2025-09-14 22:07:48 -0500

    Delete PRA4/ExcepcionNumero.java

--- PRA4/ExcepcionNumero.java
+++
@@ -1,9 +0,0 @@
-package ejerciciouno;
-
-public class ExcepcionNumero extends Exception {
-	private static final long serialVersionUID = 1L;
-
-    public ExcepcionNumero(char c) {
-        super("Se ingresó un número: " + c);
-    }
-}
