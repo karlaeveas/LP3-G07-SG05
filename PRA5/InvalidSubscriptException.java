a1a5735671f5d5a5fc74c322d40db641648f3c9d
Alexanderandres23 <rt.andres23@gmail.com>
2025-09-28 22:35:04 -0500

    Delete PRA5/InvalidSubscriptException.java

--- PRA5/InvalidSubscriptException.java
+++
@@ -1,9 +0,0 @@
-package actividaduno;
-
-public class InvalidSubscriptException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public InvalidSubscriptException(String mensaje) {
-        super(mensaje);
-    }
-}
